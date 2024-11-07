827f874f98d3f01fd11377aa86628ed8b76dd278
Suikyoo <judeanthony02sayson@gmail.com>
2024-11-07 18:36:34 +0800

    I have trust issues with this laptop

--- main.c
+++ main.c
@@ -2,39 +2,66 @@
 #include <time.h>
 #include <stdlib.h>
 
+enum PLACE;
+enum STATUS;
+struct Dialogue;
+struct Heroine;
+struct Event;
+
+typedef enum PLACE PLACE;
+typedef enum STATUS STATUS;
+typedef struct Dialogue Dialogue;
+typedef struct Heroine Heroine;
+typedef struct Event Event;
+
 //so yah, my guess was right, enums are basically like arrays
 //an enum variable with the value of the first value declared in the enum would return an integer of 0 (first item). This is great since I can randomize the location
-enum location {
+enum PLACE {
     CAFETERIA,
     PLAZA,
     HOUSE
+
 };
 
-enum heroine_status {
-
+enum STATUS {
+    STRANGER,
+    ACQUAINTANCE
 
 };
 
 struct Dialogue {
     char text[200];
     //there are three choices for the dialogue which points to other dialogues
-    struct Dialogue choices[3];
+    Dialogue* choices[3];
 
 };
 
 struct Heroine {
-    struct Dialogue dialogue;
-    int love_meter;
-    enum heroine_status;
+    Dialogue* dialogue;
+    int love_meter = 0;
+    STATUS status;
 
 };
 
+struct Event {
+    PLACE place;
+    Heroine* heroine;
+    Event* next;
+};
+
+void connect_events(Event* before, Event* after) {
+    Event* temp = before->next;
+    before->next = after;
+    after->next = temp;
+}
+
+Event* get_next_event(Event* event) {
+    return event->next;
+}
+
+
 int main(){
     srand(time(NULL));
-    enum location place;
-    place = Cafeteria;
-    printf("%d", place);
-
 
     return 0;
 
